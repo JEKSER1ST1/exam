d0bb0efe49784ee0b4728bad85191c351d847f14
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-11-27 16:02:31 +0200

    Add files via upload

---
+++ name1.py
@@ -0,0 +1,101 @@
+#1
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+    if i<5:
+        print(i)
+#2 
+#import random
+#n=int(input("Введите число --   "))
+#b=[[random.randit(0,15) for r in range(n)
+#    print(b)
+#   print(b[0])
+#   print(b[-1])
+#   c=1
+
+#   for i in b:
+#   i*=c
+#   print(i)
+#
+#
+
+
+#3
+#N =[386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+#    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217]
+#for i in N:
+#    if i%2==0:
+#        print(i)
+#    elif i==237:
+#        break
+#print(i)
+
+#4
+age=int(input("Введите возраст --   "))
+if age<2:
+    print("младенец")
+elif age>=2 and age<4:
+    print("малыш")
+elif age>=4 and age<13:
+    print("ребенок")
+elif age>=13 and age<20:
+    print("подросток")
+elif age>=20 and age<65:
+    print("взрослый")
+else:
+    print("пожилой человек")
+
+#5
+x=["sad","sad","sad"]
+V=[]
+while z:
+    del_users=z.pop(0)
+    b.append(del_users)
+
+
+#6
+food['pizza', 'falafel', 'carrot cake']
+F=food[:]
+print(food)
+print(F)
+n=(input("Блюдо--- ")
+F.append(n)
+print(F)
+
+#7
+lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+   for i in lst:
+       if lst<30 and lst%3==0
+           print(i)
+       else:
+           0+=i
+           print(i)
+   
+#8
+
+y={'a': 2, 'b': 4, 'c': 6, 'd': 8}
+for x,y in y.items():
+if i>2:
+print(y)
+
+#11
+dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+
+
+#12
+import random
+n=int(input("Введите количество ---:    "))
+m=int(input("Введите количество ---:    "))
+A=[[random.randint(1,9) for i in range(m)] for r in range(n)]
+print(A)
+for i in A:
+    print(i)
+c=[A[r][i] for r in range(n) for i in range(m)if r==i]
+print(f"главная диагональ --  {c} ")
+W=int(input("=   "))
+b=[A[W-1][i] for r in range]
+
+print(f"Індекс -- {b}")
+
+
+   
